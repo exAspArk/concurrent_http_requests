017157b6f15268d32a5af79349047c47b224ce83
exAspArk <exaspark@gmail.com>
2016-05-20 12:44:11 +0300

    Benchmark in threads

--- benchmark.rb
+++ benchmark.rb
@@ -5,7 +5,9 @@ REPEAT_COUNT = 10
 URL = 'http://google.com'
 
 Benchmark.ips do |x|
-  x.report('Sequential') do
+  x.warmup = 0
+
+  x.report('Sequential Net::HTTP') do
     REPEAT_COUNT.times do
       url = URI.parse(URL)
       request = Net::HTTP::Get.new(url)
@@ -13,7 +15,17 @@ Benchmark.ips do |x|
     end
   end
 
-  x.report('Threads') {} # TODO
+  x.report('Threads Net::HTTP') do
+    REPEAT_COUNT.times do
+      thread = Thread.new do
+        url = URI.parse(URL)
+        request = Net::HTTP::Get.new(url)
+        Net::HTTP.start(url.host, url.port) { |http| http.request(request) }
+      end
+
+      thread.join
+    end
+  end
 
   x.report('Typhoeus') {} # TODO
 
